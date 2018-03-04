f8f3880ef6e4a29a98fa21669fdcf0cc2bcbd850
76746c79 <33970430+76746c79@users.noreply.github.com>
2018-03-04 10:20:32 -0500

    Add previous work completed
    -Models from Brackeys tutorials
    -Basic playground environment scene
    -Shaders and lights
    -Walking player
    -Basic 3rd person camera with zooming in
    -Collision with player on environment
    -Crappy projectile firing

---
+++ Battles of Nayadar/Assets/Controllers/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform cameraTarget;
+
+    private float thirdPersonFactorMax;
+    private float thirdPersonFactorMin;
+    private float thirdPersonFactor;
+    private float mouseSensitivity = 6f;
+    private float yaw = 0f;
+    private float pitch = 0f;
+    private float xin, yin;
+
+    void Start()
+    {
+        thirdPersonFactorMax = 2.75f;
+        thirdPersonFactorMin = thirdPersonFactorMax * 0.75f;
+        thirdPersonFactor = thirdPersonFactorMax;
+}
+
+    void Update()
+    {
+        xin = Input.GetAxis("Mouse X");
+        yin = Input.GetAxis("Mouse Y");
+        yaw += xin * mouseSensitivity;
+        pitch -= yin * mouseSensitivity;
+
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0);
+
+        if (Input.GetMouseButton(1))
+        {
+            thirdPersonFactor = Mathf.Lerp(thirdPersonFactor, thirdPersonFactorMin, 0.1f);
+        }
+        else
+        {
+            thirdPersonFactor = Mathf.Lerp(thirdPersonFactor, thirdPersonFactorMax, 0.1f);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, cameraTarget.position - transform.forward * thirdPersonFactor, 0.95f);
+    }
+
+}
---
+++ Battles of Nayadar/Assets/Controllers/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rig;
+    private float speed = 4f;
+    public new Transform camera = null;
+    public GameObject arrowPrefab;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+        float diagspeed = Mathf.Sqrt(2f * speed * speed) / 2f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                //forward
+                movement += transform.forward * diagspeed * Time.deltaTime;
+                //left
+                movement += Quaternion.Euler(0, -90, 0) * (transform.forward * diagspeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                //forward
+                movement += transform.forward * diagspeed * Time.deltaTime;
+                //right
+                movement += Quaternion.Euler(0, 90, 0) * (transform.forward * diagspeed * Time.deltaTime);
+            }
+            else
+            {
+                movement += transform.forward * speed * Time.deltaTime;
+            }
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                //backward
+                movement += Quaternion.Euler(0, 180, 0) * transform.forward * diagspeed * Time.deltaTime;
+                //left
+                movement += Quaternion.Euler(0, -90, 0) * (transform.forward * diagspeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                //backward
+                movement += Quaternion.Euler(0, 180, 0) *transform.forward * diagspeed * Time.deltaTime;
+                //right
+                movement += Quaternion.Euler(0, 90, 0) * (transform.forward * diagspeed * Time.deltaTime);
+            }
+            else
+            {
+                movement += Quaternion.Euler(0, 180, 0) * transform.forward * speed * Time.deltaTime;
+            }
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            movement += Quaternion.Euler(0, -90, 0) * (transform.forward * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            movement += Quaternion.Euler(0, 90, 0) * (transform.forward * speed * Time.deltaTime);
+        }
+
+        rig.MovePosition(transform.position + movement);
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, camera.eulerAngles.y, transform.eulerAngles.z);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+
+            GameObject arrow = Instantiate(arrowPrefab, transform.position + new Vector3(0, 0.5f, 0) + transform.forward, transform.rotation);
+            Rigidbody arrowRig = arrow.GetComponent<Rigidbody>();
+            arrow.transform.eulerAngles = new Vector3(transform.eulerAngles.x, camera.eulerAngles.y, transform.eulerAngles.z);
+            arrowRig.velocity = transform.forward * 20f;
+        }
+    }
+
+}
